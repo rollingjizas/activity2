e35c858bd2ca40585d5078b10787891032e44e9a
Tiansing <christian_igbaras@yahoo.com>
2022-10-07 00:10:56 +0800

    login style fixed

--- index.php
+++ index.php
@@ -1,23 +1,40 @@
-<?php 
+<?php
 
 include "iwishiwasheader.php";
 ?>
-<body>
-		<form action="LOGIN.php" method="POST"><br><br><br><br><br><br><br><br>
-			<div class="container text-center border border-5"><br>
-			<div class="h4 pb-2 mb-4 text-dark border-bottom border-success">Information Management System - Activity 2 in ADV03</div>
-			<div></div>
-			<?php if (isset($_GET['error'])) { ?>
-					<p class="error text-danger"><?php echo $_GET['error']; ?></p>
-			<?php }?>
-			<label>Surname: </label>
-			<input type="text" name="surname" placeholder="surname"><br><br>
-			<label>Password: </label>
-			<input type="password" name="password" placeholder="password"><br><br>
-			<button type="submit" name="submit" class="btn btn-light btn-outline-success float-middle">Submit</button>
-			<div class="h4 pb-2 mb-4 text-dark border-bottom border-success"></div><br>
-			</div>
-		</form>
 
+
+<!DOCTYPE html>
+<html>
+
+<head>
+	<link rel="stylesheet" href="style.css">
+	<title>login </title>
+</head>
+
+<body>
+
+	<body>
+		<div class="login-page">
+			<div class="form">
+				<div class="login">
+					<div class="login-header">
+						<h3>LOGIN</h3>
+						<p>Information Management System - Activity 2 in ADV03.</p>
+					</div>
+				</div>
+				<form class="login-form" form action="LOGIN.php" method="POST">
+					<?php if (isset($_GET['error'])) { ?>
+						<p class="error text-danger"><?php echo $_GET['error']; ?></p>
+					<?php } ?>
+					<input type="text" placeholder="username" name="surname" />
+					<input type="password" placeholder="password" name="password" />
+					<button type="submit" name="submit" class="btn btn-light btn-outline-success float-middle">Submit</button>
+					<p class="message">Not registered? <a href="#">Create an account</a></p>
+				</form>
+			</div>
+		</div>
+	</body>
 </body>
+
 </html>
