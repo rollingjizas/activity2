f22b3b4ca35eb3cd0cc9d8144c9cecd35d2f2a8b
Tiansing <christian_igbaras@yahoo.com>
2022-10-15 14:12:59 +0800

    remote connection fixed

--- configure.php
+++ configure.php
@@ -12,14 +12,24 @@
 
 
 // =============FOR REMOTE CONNECTION===========
-$db["db_host"] = "remotemysql.com";
-$db["db_user"] = "p9Lesildoe";
-$db["db_pass"] = "JMwZj8ICYX";
-$db["db_name"] = "p9Lesildoe";
+// $db["db_host"] = "remotemysql.com";
+// $db["db_user"] = "p9Lesildoe";
+// $db["db_pass"] = "JMwZj8ICYX";
+// $db["db_name"] = "p9Lesildoe";
 
-foreach ($db as $key => $value) {
-	define(strtoupper($key), $value);
+// foreach ($db as $key => $value) {
+// 	define(strtoupper($key), $value);
+// }
+// global $conn;
+// $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS);
+
+
+$sname = "remotemysql.com";
+$uname = "p9Lesildoe";
+$pass = "JMwZj8ICYX";
+$db_name = "p9Lesildoe";
+
+$conn = mysqli_connect($sname, $uname, $pass, $db_name);
+if (!$conn) {
+	echo "CONNECTION FAILED!!!";
 }
-global $connection;
-$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS);
-?>
